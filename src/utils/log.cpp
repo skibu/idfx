f7b4fbc5d8452126717ccd93ae7f50f70f54b9c9
Michael Smith (SynthBu) <msmithtransit@gmail.com>
2025-03-23 13:02:08 -0700

    Create log.cpp

---
+++ src/utils/log.cpp
@@ -0,0 +1,29 @@
+/*
+ * SPDX-License-Identifier: MIT
+ */
+
+#include "log.hpp"
+
+#include "freertos/FreeRTOS.h" // FIXME needed?
+#include "freertos/task.h" // FIXME needed?
+
+
+std::string idfx::threadId() {
+    // Note: For ESP cannot use regular pthread info for the "main" thread.
+    // This is due to FreeRTOS not using pthreads. Therefore using FreeRTOS
+    // functions to get the current task's name.
+    TaskHandle_t task_handle = xTaskGetCurrentTaskHandle();
+    char *thread_name =  pcTaskGetName(task_handle);
+    return std::string(thread_name);
+}
+
+std::string idfx::shortThreadId() {
+    const int MAX_CHARS = 8;
+    std::string id = thread_id();
+    if (id.size() < MAX_CHARS) {
+        return id;
+    } else {
+        return id.substr(id.size() - MAX_CHARS);
+    }
+}
+
